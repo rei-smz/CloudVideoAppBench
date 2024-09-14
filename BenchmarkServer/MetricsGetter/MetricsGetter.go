77256cc055d383956af136cfe9c0d65879a59f2b
Rin Shimizu <52402142+rei-smz@users.noreply.github.com>
2024-09-13 21:13:14 -0400

    Update metrics getter

--- BenchmarkServer/MetricsGetter/MetricsGetter.go
+++ BenchmarkServer/MetricsGetter/MetricsGetter.go
@@ -2,15 +2,77 @@ package MetricsGetter
 
 import (
 	"log"
+	"os"
+	"time"
 )
 
 type MetricsGetter interface {
-	Run()
+	Run(recordMethod func() string)
 	Start(fileName string) bool
 	Stop() bool
+	recordMetrics() string
 }
 
-// NewMetricsGetter is a factory function creating an object of the concrete classes implementing interface MetricsGetter
+type metricsGetter struct {
+	controlCh  chan bool
+	fileNameCh chan string
+	retCh      chan bool
+}
+
+func (g *metricsGetter) Start(fileName string) bool {
+	g.controlCh <- true
+	g.fileNameCh <- fileName
+	return <-g.retCh
+}
+
+func (g *metricsGetter) Stop() bool {
+	g.controlCh <- false
+	return <-g.retCh
+}
+
+func (g *metricsGetter) Run(recordMethod func() string) {
+	log.Println("MetricsGetter started")
+	running := false
+	var file *os.File
+
+	for {
+		select {
+		case ctrl := <-g.controlCh:
+			if ctrl {
+				if file != nil {
+					file.Close()
+				}
+				var err error
+				file, err = os.Create(<-g.fileNameCh)
+				if err != nil {
+					file.Close()
+					g.retCh <- false
+				} else {
+					g.retCh <- true
+					running = true
+					log.Println("Metrics getter is running")
+				}
+			} else {
+				err := file.Close()
+				running = false
+				log.Println("Metrics getter is stopped")
+				if err != nil {
+					g.retCh <- false
+				} else {
+					file = nil
+					g.retCh <- true
+				}
+			}
+		default:
+			if running {
+				file.WriteString(recordMethod())
+				time.Sleep(15 * time.Second)
+			}
+		}
+	}
+}
+
+// NewMetricsGetter is a factory function creating an object of the concrete classes implementing interface metricsGetter
 func NewMetricsGetter(metricsType string) MetricsGetter {
 	//metricsType := os.Getenv("METRICS_TYPE")
 
